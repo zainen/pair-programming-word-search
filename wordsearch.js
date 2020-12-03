5452afe0325d9b57b389965a9e0d3214abc67a89
zainen <zainensuzuki@gmail.com>
2020-12-03 05:03:38 +0000

    pair programing with Colin

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,26 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    let newArray = []
+    for (let x = 0; x < letters.length; x++) {
+        newArray.push([])
+    }
+    for (let y = 0; y < letters.length; y++) {
+        for (let x = 0; x < letters[y].length; x++) {
+            newArray[x][y] = letters[y][x];
+        }
+    }
+    const verticalJoin = newArray.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 module.exports = wordSearch
+
+
+
